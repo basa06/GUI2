8ca54bece7a0d48330e126a7d4b536f7e10842a2
basa06 <basa.akshaya@syk.fi>
2024-01-30 09:09:27 +0200

    new changes

--- src/GUI.java
+++ src/GUI.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionEvent;
 
 import java.util.Timer;
 import java.util.TimerTask;
+
 public class GUI extends JFrame {
     // constructor will create all GUI components and set their properties
 
@@ -14,17 +15,17 @@ public class GUI extends JFrame {
 
         super("A simple GUI demo");
 
-        final Object stateLock = new Object();
+
         // frame properties
         setSize(400, 300);
         setLayout(new FlowLayout());
-        setVisible(true);
+
         JMenuBar menuBar;
 
         menuBar = new JMenuBar();
         JMenu fileMenu = new JMenu("File");
         JMenuItem quitItem = new JMenuItem("Quit");
-        quitItem.addActionListener((ActionEvent e) -> confirm_exit());
+        quitItem.addActionListener((ActionEvent e) ->{});
 
         fileMenu.add(quitItem);
         menuBar.add(fileMenu);
@@ -34,33 +35,25 @@ public class GUI extends JFrame {
         setVisible(true);
 
 
-        int state = 0;
-        Timer timer = new Timer();
-        TimerTask task = new stateincrament();
 
-        timer.schedule(task, 0, 100);
+        Timer timer = new Timer(1000, (ActionEvent e) -> state = state + 1);
+
 
         String value_state = state + "";
         JLabel stateLabel = new JLabel(value_state);
         add(stateLabel);
 
-
-        public void confirm_exit () {
-            int result = JOptionPane.showConfirmDialog(this, "Are you sure you want to exit?", "Exit Confirmation", JOptionPane.YES_NO_OPTION);
-            if (result == JOptionPane.YES_OPTION) {
-                System.exit(0);
-            }
-
-        }
-
-
-
-        class stateincrament extends TimerTask {
-            public int state = 0;
-
-            public void run() {
-                state++;
-            }
-        }
     }
+    int state = 0;
+    public void confirm_exit() {
+        int result = JOptionPane.showConfirmDialog(this, "Are you sure you want to exit?", "Exit Confirmation", JOptionPane.YES_NO_OPTION);
+        if (result == JOptionPane.YES_OPTION) {
+            System.exit(0);
+        }
+
+    }
+
+
+}
+
 
